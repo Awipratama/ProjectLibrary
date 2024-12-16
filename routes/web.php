1a41b8eca22aeb86f65b4d715fdc3e38f3f08aee
Awipratama <awipratama05122006@gmail.com>
2024-12-16 20:32:38 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ Route::get('/userdashboard', function () {
     return view('userDashBoard');
 });
 
-// Route::get('/login', [App\Http\Controllers\PageController::class, 'login'])->name('login');
 Auth::routes();
 
 Auth::routes();
